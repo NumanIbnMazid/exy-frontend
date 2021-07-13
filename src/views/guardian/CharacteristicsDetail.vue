2d35223d39ea5eb7cbac62ab7e35fd9d48458697
Mahmudul Hasan <mahmudulhasan661@gmail.com>
2021-07-13 22:49:34 +0600

    guardian Charactarestic Overview Tab done 100%

--- src/views/guardian/CharacteristicsDetail.vue
+++ src/views/guardian/CharacteristicsDetail.vue
@@ -29,13 +29,13 @@
             </ul>
           </div>
         </div>
-        <div class="col-md-6 text-center box-border">
-          <p class="text-sm m-0 pt-2">この園へ問い合わせる</p>
+        <div class="col-md-6 text-center box-border pt-1">
+          <a href="#" class="text-sm">この園へ問い合わせる</a>
         </div>
       </div>
     </section>
 
-    <!-- details-tab start -->
+    <!-- details-section start -->
     <section class="charecteristics-details">
       <div class="container">
         <div class="row">
@@ -2002,8 +2002,52 @@
         </div>
       </div>
       <!-- Word Of Mouth Tab End -->
+
+      <!-- Overview Tab Start -->
+      <div v-if="activeTab == 5" class="overview">
+        <div class="row w-100 ms-0 pt-2">
+          <div class="col-md-11 left-col mt-5">
+            <div class="row row-1">
+              <div class="col-md-2 left">
+                <p class="text-center">会社概要</p>
+              </div>
+              <div class="col-md-10 right">
+                <p>株式会社EXY <span class="span-padding">代表取締役 野村瑛莉</span></p>
+                <p>
+                  〒550-0014 大阪府大阪市西区北堀江1-2-27 AXIS SOUTH 四ツ橋ビル407号室 <br>
+                  TEL：06-6539-8484 <span class="ps-5">FAX：06-6539-8485</span>
+                </p>
+                <p class="m-0">営業時間：平日 9：00〜18：00</p>
+              </div>
+            </div>
+            <div class="row row-2">
+              <div class="col-md-2 left">
+                <p class="text-center">キャンセルについて</p>
+              </div>
+              <div class="col-md-10 right">
+                <p>当園では以下の条件の通りです。詳細は、各リンク先をご確認ください。</p>
+                <p class="text-decoration-underline">
+                  ・お客様都合によるキャンセル<br>
+                  ・当園都合によるキャンセル
+                </p>
+                <p>
+                  ▼対応条件 <br>
+                  対応期間内に電話もしくはメールにてご連絡いただいたもののみ原則対応いたします。
+                </p>
+                <p>▼対応可能期間ご予約日より○日前まで</p>
+                <p>▼返金額</p>
+                <p class="m-0">▼備考</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-1 right-col pe-0">
+            <a href="#" class="philosophy-right pull-right text-center h-60">予約へ進む</a>
+          </div>
+        </div>
+      </div>
+      <!-- Overview Tab End -->
     </section>
-    <!-- details-tab end -->
+    <!-- details-section end -->
   </div>
 </template>
 
