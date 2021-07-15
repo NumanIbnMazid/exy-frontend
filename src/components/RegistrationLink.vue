6aea7d1c50d30e58ce9d7ca50f923ef441c82297
Motaleb Hossain <34369086+bdmotaleb@users.noreply.github.com>
2021-07-16 02:42:01 +0600

    Thoughts of Kigarni Hoik done

---
+++ src/components/RegistrationLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-link :to="{name:'Registration'}">
+    <img src="../assets/images/sinkitouroku.png" alt="EXY" class="img-fluid rounded">
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "RegistrationLink"
+}
+</script>
+
+<style scoped>
+
+</style>
