e31179e8f6f71bc3fee8d19f92c9029af37bd988
Mahmudul Hasan <mahmudulhasan661@gmail.com>
2021-07-19 23:11:13 +0600

    My Page Home section done(Page 53) done 100%

--- src/views/myPage/Home.vue
+++ src/views/myPage/Home.vue
@@ -12,7 +12,9 @@
                 <p class="m-0 text-xl fw-bold">そらくんママ</p>
               </div>
               <div class="row mt-2 ps-1">
-                <p class="m-0 text-base fw-bold"><span class="rounded-circle me-3 px-2 pt-2 pb-1 nav-badge text-center">P</span>500</p>
+                <a href="#">
+                  <p class="m-0 text-base fw-bold"><span class="rounded-circle me-3 px-2 pt-2 pb-1 nav-badge text-center">P</span>500</p>
+                </a>
               </div>
             </div>
           </div>
@@ -67,15 +69,15 @@
       <div class="row row-2">
         <div class="col-md-12 p-0">
           <div class="row row-inner align-items-center">
-            <div class="col-md-2">
+            <div class="col-md-1 m-0 p-0">
               <img src="../../assets/images/mypage/Mask_Group_41.png" alt="EXY" class="w-100">
             </div>
-            <div class="col-md-7">
-              <p class="text-lg fw-bold mb-4">COCOROHOIKUEN 弁天町園</p>
-              <p class="text-base mb-4">
+            <div class="col-md-8 ps-4">
+              <p class="text-lg fw-bold">COCOROHOIKUEN 弁天町園</p>
+              <p class="text-base">
                 <span class="spna-badge me-4">利用プラン</span> 通常プラン  <span class="px-2">|</span>  １歳児・２歳児
               </p>
-              <p class="text-base mb-4">
+              <p class="text-base">
                 <span class="spna-badge me-4">利用開始日</span> 2021/6/30 〜 
               </p>
               <p class="text-base">
@@ -101,7 +103,7 @@
           <p class="m-0 pt-1 pb-3"><span class="span-1">予約前日のお知らせ</span></p>
           <p class="text-lg text-decoration-underline">2月16日来園予定で確定している予定がございます。予定日1日前になりましたのでお知らせいたし…</p>
           <p class="p-bottom text-sm m-0">
-            <span class="pull-left">COCOROHOIKUEN 弁天町園</span> <span class="pull-right">2021年2月12日(金)</span> <span class="pull-right">11：00</span>
+            <span class="pull-left">COCOROHOIKUEN 弁天町園</span> <span class="pull-right">11：00</span> <span class="pull-right pe-4">2021年2月12日(金)</span>
           </p>
         </div>
       </div>
@@ -113,7 +115,7 @@
           <p class="m-0 pt-1 pb-3"><span class="span-2">予約確定</span></p>
           <p class="text-lg text-decoration-underline">【月極保育】ご予約が確定いたしました。田中様 この度は当園にご予約いただき誠にありがとう…</p>
           <p class="p-bottom text-sm m-0">
-            <span class="pull-left">COCOROHOIKUEN 弁天町園</span> <span class="pull-right">2021年2月12日(金)</span> <span class="pull-right">11：00</span>
+            <span class="pull-left">COCOROHOIKUEN 弁天町園</span> <span class="pull-right">11：00</span> <span class="pull-right pe-4">2021年2月12日(金)</span>
           </p>
         </div>
       </div>
@@ -125,9 +127,138 @@
           <p class="m-0 pt-1 pb-3"><span class="span-3">予約リクエスト中</span></p>
           <p class="text-lg text-decoration-underline">【月極保育】ご予約のリクエストを受付ました。現時点では、ご予約は【まだ確定しておりません…</p>
           <p class="p-bottom text-sm m-0">
-            <span class="pull-left">COCOROHOIKUEN 弁天町園</span> <span class="pull-right">2021年2月12日(金)</span> <span class="pull-right">11：00</span>
+            <span class="pull-left">COCOROHOIKUEN 弁天町園</span>  <span class="pull-right">11：00</span> <span class="pull-right pe-4">2021年2月12日(金)</span>
           </p>
         </div>
+      </div>
+      <div class="row pt-4 pull-right">
+        <a class="pull-right row-bottom text-base text-decoration-underline">お知らせ一覧 ＞</a>
+      </div>
+      <div class="row row-5">
+        <p class="m-0  text-2xl fw-bold pb-2"><span class="p-heading fw-normal">#</span> お気に入り園</p>
+      </div>
+      <a href="#">
+        <div class="row row-6">
+          <div class="left-col">
+            <img src="../../assets/images/svg/border-heart.svg" alt="EXY" class="top-border">
+          </div>
+          <div class="right-col py-3 ps-0">
+          <div class="row ps-0 pe-5">
+            <div class="col-md-2 ps-0">
+              <img src="../../assets/images/mypage/Mask_Group_45.png" alt="EXY" class="w-100">
+            </div>
+            <div class="col-md-10 pe-4">
+              <p class="text-xl fw-bold text-decoration-underline">COCOROHOIKUEN 弁天町園</p>
+              <p class="text-sm pb-2">
+                <img src="../../assets/images/svg/location.svg" alt="EXY">  大阪市大阪市港区市岡元町2-11-30 1階 
+                <span class="span-button px-2 ms-4 py-1">MAP ></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fw-bold rv-point">5.0</span>
+              </p>
+              <p class="text-sm pb-4">「えいごとにほんごであそぼう」をテーマに、楽しく自然に英語を身につけることを目標にした保育園</p>
+              <p class="p-button text-center py-2">空き確認</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="row row-6 mt-3">
+          <div class="left-col">
+            <img src="../../assets/images/svg/border-heart.svg" alt="EXY" class="top-border">
+          </div>
+          <div class="right-col py-3 ps-0">
+          <div class="row ps-0 pe-5">
+            <div class="col-md-2 ps-0">
+              <img src="../../assets/images/mypage/Mask_Group_45.png" alt="EXY" class="w-100">
+            </div>
+            <div class="col-md-10 pe-4">
+              <p class="text-xl fw-bold text-decoration-underline">COCOROHOIKUEN 弁天町園</p>
+              <p class="text-sm pb-2">
+                <img src="../../assets/images/svg/location.svg" alt="EXY">  大阪市大阪市港区市岡元町2-11-30 1階 
+                <span class="span-button px-2 ms-4 py-1">MAP ></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fw-bold rv-point">5.0</span>
+              </p>
+              <p class="text-sm pb-4">「えいごとにほんごであそぼう」をテーマに、楽しく自然に英語を身につけることを目標にした保育園</p>
+              <p class="p-button text-center py-2">空き確認</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </a>
+      <a href="#">
+        <div class="row row-6 mt-3">
+          <div class="left-col">
+            <img src="../../assets/images/svg/border-heart.svg" alt="EXY" class="top-border">
+          </div>
+          <div class="right-col py-3 ps-0">
+          <div class="row ps-0 pe-5">
+            <div class="col-md-2 ps-0">
+              <img src="../../assets/images/mypage/Mask_Group_45.png" alt="EXY" class="w-100">
+            </div>
+            <div class="col-md-10 pe-4">
+              <p class="text-xl fw-bold text-decoration-underline">COCOROHOIKUEN 弁天町園</p>
+              <p class="text-sm pb-2">
+                <img src="../../assets/images/svg/location.svg" alt="EXY">  大阪市大阪市港区市岡元町2-11-30 1階 
+                <span class="span-button px-2 ms-4 py-1">MAP ></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fa fa-star checked"></span>
+                <span class="pull-right fw-bold rv-point">5.0</span>
+              </p>
+              <p class="text-sm pb-4">「えいごとにほんごであそぼう」をテーマに、楽しく自然に英語を身につけることを目標にした保育園</p>
+              <p class="p-button text-center py-2">空き確認</p>
+            </div>
+          </div>
+          </div>
+        </div>
+      </a>
+      <div class="row pt-4 pull-right">
+        <a class="pull-right row-bottom text-base text-decoration-underline">お気に入り園一覧 ＞</a>
+      </div>
+      <div class="row row-7">
+        <p class="m-0 fw-bold text-2xl pb-2"><span class="span-color fw-normal">#</span> 子育て相談</p>
+      </div>
+      <div class="row row-8">
+        <p class="text-base mb-4 p-text-color">
+          <span class="text-sm span-button me-5">回答受付中</span> 投稿日 ：2021年2月16日 12:00
+        </p>
+        <p class="text-xl fw-bold ps-4">子供がなかなか寝てくれない</p>
+        <p class="m-0 text-lg">
+          ▶︎【相談内容】親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。
+        </p>
+      </div>
+      <div class="row row-8">
+        <p class="text-base mb-4 p-text-color">
+          <span class="text-sm span-button me-5">回答受付中</span> 投稿日 ：2021年2月16日 12:00
+        </p>
+        <p class="text-xl fw-bold ps-4">子供がなかなか寝てくれない</p>
+        <p class="m-0 text-lg">
+          ▶︎【相談内容】親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。
+        </p>
+      </div>
+      <div class="row row-8">
+        <p class="text-base mb-4 p-text-color">
+          <span class="text-sm span-button me-5">回答受付中</span> 投稿日 ：2021年2月16日 12:00
+        </p>
+        <p class="text-xl fw-bold ps-4">子供がなかなか寝てくれない</p>
+        <p class="m-0 text-lg">
+          ▶︎【相談内容】親譲りの無鉄砲で小供の時から損ばかりしている。小学校に居る時分学校の二階から飛び降りて一週間ほど腰を抜かした事がある。なぜそんな無闇をしたと聞く人があるかも知れぬ。
+        </p>
+      </div>
+       <div class="row pt-4 pull-right">
+        <a class="pull-right row-bottom text-base text-decoration-underline">子育て・保育相談 ＞</a>
       </div>
     </section>
   </div>
