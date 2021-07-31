1336bf3150a51574972666e845670dde20f1095e
NumanIbnMazid <numanibnmazid@gmail.com>
2021-07-31 14:30:29 +0600

    Page 76 completed

--- src/views/childcare/TabContent.vue
+++ src/views/childcare/TabContent.vue
@@ -38,6 +38,16 @@
                                     <div class="col-6 text-start">
                                         <span>2021/03/09</span>
                                         <span class="ms-3">15:11</span>
+                                        <span class="ms-3" v-if="i < 5 && i > 2">
+                                            <span class="badge rounded-circle kigaruni-yellow-background">p</span>
+                                            <span class="ms-1 kigaruni-yellow">100pt</span>
+                                        </span>
+                                        <span class="ms-3 rounded-pill bg-secondary text-white ps-3 pe-3 pt-1 pb-1" v-if="i == 5">
+                                            <small>回答リクエスト</small>
+                                        </span>
+                                        <span class="ms-3 rounded-pill bg-white text-dark ps-4 pe-4 pt-1 pb-1 border border-1 border-secondary" v-if="i == 5">
+                                            <small>匿名投稿</small>
+                                        </span>
                                     </div>
                                     <div class="col-6 text-end">
                                         <span><u>＃回答受付中</u></span>
@@ -68,9 +78,88 @@
 
                 <!-- section 2 -->
                 <div>
-                    <h1>Section 2</h1>
+                    <p class="fw-bold text-xl">自分の子育て相談アンサー</p>
+                    <!-- item -->
+                    <div class="mt-5">
+                        <div v-for="i in 5" :key="i">
+                            <div class="border border-1 rounded-3 pb-3 ps-3 pe-3 shadow" :class="i != 1 ? 'mt-4':''">
+                                <div class="text-end overlay-badge">
+                                    <span class="bg-cb3a82 ps-5 pe-5 pt-2 pb-2 text-lg text-white">回答受付中</span>
+                                </div>
+                                <div class="border border-1 rounded-3 p-3 mt-3">
+                                    <div>
+                                        <u class="fw-bold">保育士をしていました。</u>
+                                    </div>
+                                    <div class="mt-2">
+                                        <p>
+                                            お家大好きなんでしょうね。質問者様は大変だと思いますが、中には2年以上朝泣いてくる子がいました！３ヶ月経ったころということも、初めに泣く子や慣れてきた頃泣く子など、様々なので特に決定的な出来事があったとかでない限り普通の行き渋りかな、と思…
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="row mt-2 ps-2 pe-3">
+                                    <div class="col-6 text-start">
+                                        <span>2021/03/09</span>
+                                        <span class="ms-3">15:11</span>
+                                    </div>
+                                    <div class="col-6 text-end" v-if="i == 1">
+                                        <span><u>＃回答受付中</u></span>
+                                        <span class="ms-3"><u>＃保育関連</u></span>
+                                        <span class="ms-3"><u>＃働くママ・パパ</u></span>
+                                        <span class="ms-3"><u>＃5歳児</u></span>
+                                        <span class="ms-3"><u>＃女の子</u></span>
+                                        <span class="ms-3 fa fa-comment text-white fa-bordered"></span>
+                                        <span class="ms-1">5</span>
+                                    </div>
+                                    <div class="col-6 text-end" v-else-if="i == 2">
+                                        <span><u>＃</u></span>
+                                        <span class="ms-3"><u>＃お悩み聞いて！</u></span>
+                                        <span class="ms-3"><u>＃旦那・家族</u></span>
+                                        <span class="ms-3"><u>＃働くママ</u></span>
+                                        <span class="ms-3"><u>＃5歳児</u></span>
+                                        <span class="ms-3 fa fa-comment text-white fa-bordered"></span>
+                                        <span class="ms-1">5</span>
+                                    </div>
+                                    <div class="col-6 text-end" v-else>
+                                        <span><u>＃解決済み</u></span>
+                                        <span class="ms-3"><u>＃お悩み聞いて！</u></span>
+                                        <span class="ms-3"><u>＃旦那・家族</u></span>
+                                        <span class="ms-3"><u>＃働くママ</u></span>
+                                        <span class="ms-3"><u>＃5歳児</u></span>
+                                        <span class="ms-3 fa fa-comment text-white fa-bordered"></span>
+                                        <span class="ms-1">5</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /item -->
+
+                    <!-- pagination -->
+                    <div class="text-center mt-5 mb-5">
+                        <span class="m-4 kigaruni-green-background p-3 rounded-circle text-white">1</span>
+                    </div>
+                    <!-- /pagination -->
                 </div>
                 <!-- /section 2 -->
+
+                <!-- section 3 -->
+                <div class="bg-light p-5">
+                    <div class="row border border-secondary border-1 bg-white ps-2 pe-2 pt-4 pb-4">
+                        <div class="col-lg-6 col-md-6 col-12 text-start">
+                            <span>あなたの子育てに関する疑問や悩みを質問してみましょう！</span>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-12 text-end">
+                            <button class="border border-secondary border-1 ps-5 pe-5 pt-2 pb-2 bg-white">
+                                <span class="ps-5 pe-5">相談する</span>
+                            </button>
+                        </div>
+                    </div>
+
+                    <div class="mt-5 crossed border border-secondary border-1 bg-white p-5 text-center">
+                        <span>他のママの疑問や悩みをみてみる</span>
+                    </div>
+                </div>
+                <!-- /section 3 -->
             </div>
         </div>
         <!-- *** /Tab 1 *** -->
